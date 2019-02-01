d454f77310df8348de984c3745a2aa0c734b7ddc
Urmat Bekboev <bek.urmat@gmail.com>
2019-02-01 12:44:19 +0600

    Full information fetch

--- src/components/Post/FullPost.js
+++ src/components/Post/FullPost.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from '../../axios-instances';
 import { Jumbotron, Button } from 'reactstrap';
 
-const FullPost = props => {
-    return (
-        <div>
-            <Jumbotron>
-                <h2 className="display-3">Title</h2>
-                <hr className="my-2" />
-                <p className="lead">Description</p>
+class FullPost extends Component {
+    state = {
+        id: null,
+        post: null,
+    };
+
+    componentDidMount() {
+        this.setState({id: this.props.match.params.id });
+        axios.get('posts/' + this.props.match.params.id + '.json').then(response => {
+            this.setState({post: response.data});
+        });
+    };
+
+    render() {
+        if (!this.state.post) return null;
+
+        return (
+            <div className={"item-" + this.state.id}>
+                <Jumbotron>
+                    <h2 className="display-3">{this.state.post.title}</h2>
+                    <hr className="my-2" />
+                    <p className="lead">{this.state.post.description}</p>
 
 
-            </Jumbotron>
-            <p className="lead float-right">
-                <Button color="primary">Edit Post</Button>
-                <Button color="danger">Delete Post</Button>
-            </p>
-        </div>
-    );
-};
+                </Jumbotron>
+                <p className="lead float-right">
+                    <Button color="primary">Edit Post</Button>
+                    <Button color="danger">Delete Post</Button>
+                </p>
+            </div>
+        );
+    }
+}
 
 export default FullPost;
