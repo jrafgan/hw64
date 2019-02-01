0514a8347781d263f2fae60defb6551b261340ef
Urmat Bekboev <bek.urmat@gmail.com>
2019-02-01 10:38:34 +0600

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route, Switch} from "react-router";
+import {Container} from "reactstrap";
+
+import Header from "./components/Header/Header";
+import AddPost from "./containers/AddPost/AddPost";
+
 import './App.css';
+import PostList from "./containers/PostList/PostList";
+import FullPost from "./components/Post/FullPost";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+          <Container>
+              <Switch>
+                  <Route path="/" exact component={PostList}/>
+                  <Route path="/posts/add" exact render={() => <AddPost />}/>
+                  <Route path="/posts/:id"  render={() => <FullPost />}/>
+                  <Route path="/about" exact render={() => <h1>About</h1>}/>
+                  <Route path="/contact" exact component={FullPost}/>
+              </Switch>
+
+          </Container>
       </div>
     );
   }
